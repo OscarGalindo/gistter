e8a7a2f5d16846518372989fec72a6f671e703ca
kaseOga <galindero@gmail.com>
2015-05-06 18:34:34 +0200

    User model validation

--- gistter/models.py
+++ gistter/models.py
@@ -8,7 +8,7 @@ import re
 def email_validator(value):
     email = re.compile(r'(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)', re.IGNORECASE)
     if not bool(email.match(value)):
-        raise ValidationError('%s is not a valid email')
+        raise ValidationError('%s is not a valid email: ' % value)
 
 
 @mongo.register
@@ -16,11 +16,11 @@ class User(Document):
     __database__ = 'gistter'
     __collection__ = 'users'
     structure = {
-        'username': unicode,
+        'username': basestring,
         'birth': datetime,
-        'email': unicode,
-        'password': unicode,
-        'description': unicode,
+        'email': basestring,
+        'password': basestring,
+        'description': basestring,
         'tweets_count': int,
         'created_at': datetime
     }
@@ -32,6 +32,7 @@ class User(Document):
 
     required_fields = ['username', 'email', 'password']
     default_values = {'tweets_count': 0, 'created_at': datetime.utcnow}
+    use_dot_notation = True
 
     def get_absolute_url(self):
         return url_for('user.index', kwargs={"username": self.username})
