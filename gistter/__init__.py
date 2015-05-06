3378c5fa88575a39deab88368ac84e9e94b5e327
kaseOga <galindero@gmail.com>
2015-05-06 18:19:09 +0200

    Errorhandler 404 + 500

--- gistter/__init__.py
+++ gistter/__init__.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import mongokit
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 from .user.user import user
+
 app.register_blueprint(user)
 mongo = mongokit.Connection()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return "<h1>%s</h1>" % error
+
+
+@app.errorhandler(500)
+def internal_error(exception):
+    return "<h1>%s</h1>" % exception
