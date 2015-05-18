e3b647573c79243ea5ea7cc519bfe3c7cb16b39d
kaseOga <galindero@gmail.com>
2015-05-18 15:55:35 +0200

    Factory para crear la app

--- gistter/__init__.py
+++ gistter/__init__.py
@@ -1,27 +1,33 @@
 from flask import Flask
-from flask.ext.jwt import JWT
 from flask.ext.mongokit import MongoKit
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-mongo = MongoKit(app)
-jwt = JWT(app)
-
-from .user.views import user
-from .auth.views import auth
-app.register_blueprint(user)
-app.register_blueprint(auth)
-
-from .user.models import User
-mongo.register([User])
+from flask.ext.cors import CORS
+from flask.ext.jwt import JWT
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return "<h1>%s</h1>" % error
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    mongo = MongoKit(app)
+    jwt = JWT(app)
+    cors = CORS(app)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return "<h1>%s</h1>" % error
 
 
-@app.errorhandler(500)
-def internal_error(exception):
-    return "<h1>%s</h1>" % exception
+    @app.errorhandler(500)
+    def internal_error(exception):
+        return "<h1>%s</h1>" % exception
+
+    return app, mongo, jwt, cors
+
+
+(app, mongo, jwt, cors) = create_app()
+
+from .user.views import user as user_blueprint
+from .auth.views import auth as auth_blueprint
+
+app.register_blueprint(user_blueprint, url_prefix="/user")
+app.register_blueprint(auth_blueprint)
