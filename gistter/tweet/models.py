87bb5629f3e9b71cf9d01868c479162bbd97657d
kaseOga <galindero@gmail.com>
2015-05-19 17:42:45 +0200

    Parsear código y guardandolo en HTML

--- gistter/tweet/models.py
+++ gistter/tweet/models.py
@@ -3,7 +3,9 @@ from flask import url_for
 from gistter import mongo
 from gistter.coremodel import Core
 from gistter.user.models import User
-
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
 @mongo.register
 class Tweet(Core):
@@ -12,6 +14,8 @@ class Tweet(Core):
         'user': User,
         'user_rt': User,
         'body': basestring,
+        'code': basestring,
+        'code_html': basestring,
         'retweet': bool,
         'favorites_count': int,
         'retweet_count': int,
@@ -36,7 +40,8 @@ class Tweet(Core):
     def bind(self, data):
         self.user = data.get('user')
         self.body = data.get('body')
-        self.body_html = data.get('body')
+        self.code = data.get('code')
+        self.code_html = highlight(data.get('code'), PythonLexer(), HtmlFormatter())
         self.hashtags = [re.sub(r"#+", "#", k) for k in set(
             [re.sub(r"(\W+)$", "", j, flags=re.UNICODE) for j in
              [i for i in data.get('body').split() if i.startswith("#")]])]
